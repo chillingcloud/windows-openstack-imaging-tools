267c1bd3ebc7a35add45b8784e854e0a36c281e6
Natthasak Vechprasit <codesonteen@gmail.com>
2019-08-06 11:13:43 +0700

    Custom Script Injection

--- UnattendResources/RunBeforeSysprep.ps1
+++ UnattendResources/RunBeforeSysprep.ps1
@@ -1,1 +1,7 @@
-'New-Item -Path C:\hello.txt' | Out-File -FilePath 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\create-hello-file.ps1'
+$localScriptPath = 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts'
+
+if(!(Test-Path $localScriptPath)) {
+    mkdir -Path $localScriptPath
+}
+
+'New-Item -Path C:\hello.txt' | Out-File -FilePath "$localScriptPath\create-hello-file.ps1"
