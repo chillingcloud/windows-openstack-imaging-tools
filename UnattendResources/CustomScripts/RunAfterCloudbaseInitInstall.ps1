fc77579a5490bc5dd76c25c29a0708f16a26d826
Natthasak Vechprasit <codesonteen@gmail.com>
2019-11-16 20:20:28 +0700

    Update

--- UnattendResources/CustomScripts/RunAfterCloudbaseInitInstall.ps1
+++ UnattendResources/CustomScripts/RunAfterCloudbaseInitInstall.ps1
@@ -9,9 +9,34 @@ function Set-AdministratorUsername {
     }
 }
 
+function Set-CloudInitPlugins {
+    param(
+        [string]$configPath = null
+    )
+    if(Test-Path $configPath) {
+        $config = Get-Content $configPath
+        $config = "$config`r`nplugins=cloudbaseinit.plugins.windows.userdata.UserDataPlugin,cloudbaseinit.plugins.common.localscripts.LocalScriptsPlugin"
+        Write-Output $config > $configPath
+    }
+}
+
+function Set-CloudInitUnattendPlugins {
+    param(
+        [string]$configPath = null
+    )
+    if(Test-Path $configPath) {
+        $config = Get-Content $configPath
+        $config = {$_ -replace "plugins=cloudbaseinit.plugins.common.mtu.MTUPlugin,cloudbaseinit.plugins.common.sethostname.SetHostNamePlugin", "plugins=cloudbaseinit.plugins.common.mtu.MTUPlugin"}
+    }
+}
+
 $configDirectory = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf"
 $configFilePath = "$configDirectory\cloudbase-init.conf"
 $configUnattendFilePath = "$configDirectory\cloudbase-init-unattend.conf"
 
 #Set-AdministratorUsername $configFilePath
 #Set-AdministratorUsername $configUnattendFilePath
+
+# Disable Cloud-Init Plugins
+Set-CloudInitPlugins $configFilePath
+Set-CloudInitUnattendPlugins $configUnattendFilePath
