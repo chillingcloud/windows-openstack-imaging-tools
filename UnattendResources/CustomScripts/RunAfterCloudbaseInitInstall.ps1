32ae85b3e328d2d65b3e256ce18466f2dbecc229
Natthasak Vechprasit <codesonteen@gmail.com>
2019-11-18 20:51:55 +0700

    Update

--- UnattendResources/CustomScripts/RunAfterCloudbaseInitInstall.ps1
+++ UnattendResources/CustomScripts/RunAfterCloudbaseInitInstall.ps1
@@ -3,7 +3,8 @@ $targetConfigDir = 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf'
 
 if((Test-Path $sourceConfigDir)) {
     # Inject Cloudbase-Init Config
-    Copy-Item -Recurse -Force $sourceConfigDir 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\'
+    Copy-Item -Recurse -Force "$sourceConfigDir\cloudbase-init.conf" "$targetConfigDir\cloudbase-init.conf"
+    Copy-Item -Recurse -Force "$sourceConfigDir\cloudbase-init-unattend.conf" "$targetConfigDir\cloudbase-init-unattend.conf"
 }
 
 Write-Host -NoNewLine "[AfterCloudbaseInitInstall] Once you done any manual configuration or don't want to config, Press any key to continue...";
